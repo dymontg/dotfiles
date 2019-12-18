88997cee59cdca1a96f458e9a58b2429d491fe24
Daniel Montgomery <dymontg@gmail.com>
2019-12-17 16:43:03 -0800

    Added i3lock.

---
+++ .scripts/lock.zsh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script copied from https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
+# and https://pastebin.com/ViRWBH90
+# Depends on: imagemagick, i3lock, scrot
+
+IMAGE=/tmp/lockscreen.png
+ICON=~/dotfiles/.config/i3/lock
+
+scrot $IMAGE
+convert $IMAGE -scale 10% -scale 1000% -fill black -colorize 25% $IMAGE
+convert $IMAGE $TEXT -gravity center -geometry +0+200 -composite $IMAGE
+convert $IMAGE $ICON -gravity center -composite -matte $IMAGE
+i3lock -u -i $IMAGE
