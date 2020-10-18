83a47812c35941e2131d2e8a0a6492d0b2a3cda5
Daniel Montgomery <dymontg@gmail.com>
2020-10-18 12:07:32 -0700

    Moved from spacemacs to doom.

--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 # If you come from bash you might have to change your $PATH.
-export PATH=/usr/lib/ccache/bin:$HOME/.local/bin:$PATH
+export PATH=/usr/lib/ccache/bin:$HOME/.local/bin:$HOME/.emacs.d/bin:$PATH
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 SCRIPTDIR=$(dirname "$(readlink -f '$0')")
-files=".Xresources .xinitrc .zshrc .spacemacs .oh-my-zsh .fonts .config/i3 .config/rofi .config/lemonbar .scripts .emacs.d"
+files=".Xresources .xinitrc .zshrc .oh-my-zsh .fonts .config/i3 .config/rofi .config/lemonbar .scripts .emacs.d .doom.d"
 
 for file in $files; do
     printf "Creating symlink to %s in home directory.\n" "${file}"
     ln -sfn "${SCRIPTDIR}/${file}" "${HOME}/${file}"
 done
+
+doom install
