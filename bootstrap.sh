d752990e95a1f436b0a637410f6aa4ac4340c0e1
Daniel Montgomery <dymontg@gmail.com>
2021-02-13 18:20:02 -0800

    Added mopidy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 SCRIPTDIR=$(dirname "$(readlink -f '$0')")
-files=".Xresources .xinitrc .zshrc .oh-my-zsh .fonts .config/i3 .config/rofi .config/lemonbar .scripts .emacs.d .doom.d"
+files=".Xresources .xinitrc .zshrc .oh-my-zsh .fonts .config/i3 .config/rofi .config/lemonbar .config/mopidy .scripts .emacs.d .doom.d"
 
 for file in $files; do
     printf "Creating symlink to %s in home directory.\n" "${file}"
     ln -sfn "${SCRIPTDIR}/${file}" "${HOME}/${file}"
 done
+
+# Handle mopidy configureation seperately
+cp "${SCRIPTDIR}"/.config/mopidy/mopidy.conf.safe "${SCRIPTDIR}"/.config/mopidy/mopidy.conf 
 
 doom install
